43948f9860d8d48fe67977c6d5a18f3bb826bb4c
Sandeep <sandeepmaraboina14s@gmail.com>
2023-03-25 11:36:32 +0000

    cricket team prctice

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqllite3 = require("sqlite3");
+
+const app = express();
+app.use(express.json());
+const path = require("path");
+
+const dbpath = path.join(__dirname, "cricketTeam.db");
+let db = null;
+
+const initalisedbnconnect = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqllite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log(`Server is running at http://CricketTeam:3000/`);
+    });
+  } catch (e) {
+    console.log(`${e.message}`);
+    process.exit(1);
+  }
+};
+
+initalisedbnconnect();
+
+/*API 1 get Details*/
+
+app.get("/players/", async (request, response) => {
+  const dbquery = `SELECT * from cricket_team;`;
+  playersArray = await db.all(dbquery);
+  response.send(
+    playersArray.map((eachPlayer) =>
+      convertDbObjectToResponseObject(eachPlayer)
+    )
+  );
+});
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+/*API 2 Add player*/
+
+app.post("/players/", async (request, response) => {
+  const playerdetails = request.body;
+
+  const { player_name, jersey_number, role } = playerdetails;
+
+  const dbaddquery = `
+    INSERT INTO
+        cricket_team (player_name,jersey_number,role)
+    VALUES
+    (
+        '${player_name}',
+        '${jersey_number}',
+        '${role}'
+    );`;
+  const dbResponse = await db.run(dbaddquery);
+  /*const playerId = dbResponse.lastID;
+  response.send({ playerId: playerId });*/
+  response.send("Player Added to Team");
+});
+
+/* API 3 Returns a player based on a player ID*/
+
+app.get("/players/:playerId/", async (request, response) => {
+  const { playerId } = request.params;
+
+  const dbquery = `
+    SELECT 
+        * 
+    FROM 
+        cricket_team 
+    WHERE
+        player_id = ${playerId};`;
+
+  const player1 = await db.get(dbquery);
+  response.send(player1);
+});
+
+module.exports = app;
